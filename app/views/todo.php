42a654156869df5f5c2ad188b475b8ffb186f4aa
Manish Bhattarai <manish.ilam@gmail.com>
2015-03-25 18:55:54 +0400

    Layouttodos

--- app/views/todo.php
+++ app/views/todo.php
@@ -20,67 +20,110 @@
     }
     </style>
 <?php echo HTML::style( 'dist/semantic.min.css')?>
+     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <?php echo HTML::script( 'dist/semantic.min.js')?>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+
 
 <?php echo HTML::script('/ajs/todo.js');?>
 
 
 
 <div class="ui center aligned grid" ng-app = 'todoList' ng-controller = 'todoController as todoC'>
-    <div class="sixteen column row">
-    <div class="three wide left aligned column">
-<form novalidate class = "ui small bu form">
-            <div class="ui small blue inverted vertical  menu">
-          <div class="item">
-            <div class="ui input"><input type="text" placeholder="Search..." ng-model="filtertext"></div>
-          </div>
-          <h5 class="ui horizontal header inverted divider">
-  <i class="add icon"></i>
-  Add New Task
-</h5>
-          
-          <a class="item">
-           
-    <input class = "ui small input" placeholder = "Title Here" type="text" ng-model="todoC.new.title"><br>
-          </a>
-          <a class="item">
-           
-    <input class = "ui small input" placeholder = "Description Here" rows = "5" type="text" ng-model="todoC.new.description"><br>
-          </a>
-          <a class="item">
-           
-    <button class="ui small green button" ng-click="todoC.add()">Add</button><br>
-          </a>
-          
-        </div>
+
+    <div class="three wide left aligned column" >
+            <form name = 'newTask' novalidate class = "ui small bu form">
+                        <div class="ui small blue inverted vertical  menu">
+                      
+                      <div class="item">
+                     
+                        <div class="ui input"><input type="text" placeholder="Search..." ng-model="filtertext" ng-change="todoC.refresh;"></div>
+                      </div>
+                      <h5 class="ui horizontal header inverted divider">
+              <i class="add icon"></i>
+              Add New Task
+            </h5>
+                      
+                      <a class="item">
+                       
+                <input class = "ui small input" name = 'title' placeholder = "Title Here" type="text" ng-model="todoC.new.Title"><br>
+                      </a>
+                      <a class="item">
+                <div class="field" >
+  
+    <textarea placeholder = "Description Here" rows = "5" type="text" ng-model="todoC.new.Description"></textarea>
+  </div>       
+                
+                      </a>
+                      <a class="item">
+                       
+                <input class = "ui small input" placeholder = "Status" rows = "5" type="text" ng-model="todoC.new.Status"><br>
+                      </a>
+                      <a class="item">
+                       
+                <button class="ui small green button" ng-click="todoC.add()">Add</button><br>
+                      </a>
+                      
+                    </div>
 
 
-            
-          </form>
+                        
+                      </form>
     </div>
 
 
     <div class="twelve wide left aligned column">
-        <div class="ui blue segment" ng-repeat = "todo in todoC.todos | filter: filtertext">
-            <h4 class="ui blue header">{{todo.Title}}</h4>
+    <div class="ui grid" >
+        
+    <div class = "five wide left aligned column" ng-repeat = "todo in todoC.todos | filter: filtertext">
+        
+        <h4 class="ui blue top attached header">
+<div class="ui top attached indicating large progress" ng-attr-data-percent="{{todo.Status}}">
+      <div class="bar"></div>
+    </div>
+    <br> 
+  {{todo.Title}}
+
+  <div class="sub header">25th March 2015</div>
+    
+</h4>
+
+        <div class="ui attached piled segment" >
+            
+
 
             <p>{{todo.Description}}</p>
+            <br>
+
+             <div class="mini blue icon ui buttons">
+  <div class="ui button"><i class="checkmark icon"></i></div>
+  <div class="ui button"><i class="close icon"></i></div>
+  <div class="ui button"><i class="share icon"></i></div>
+  
+</div>
 
         </div>
+       
+    </div>
 
-        <div class="ui blue segment">
-            <h4 class="ui blue header">{{todoC.new.title}}</h4>
-
-            <p>{{todoC.new.description}}</p>
+    <div class = "three wide left aligned column" ng-show ="newTask.title.$dirty" >
+        <div class="ui segment">
+            <h4 class="ui blue header">{{todoC.new.Title}}</h4>
+            
+            <p>{{todoC.new.Description}}</p>
 
         </div>
     </div>
 
+    </div>
+    </div>
 
-
-
+    
 
 </div>
-</div>
 
+<script>
+    $(document).ready(function(){
+        $('.progress').progress();
+    });
+</script>
--- public/ajs/todo.js
+++ public/ajs/todo.js
@@ -3,16 +3,26 @@ var app = angular.module("todoList",[]);
 
 app.controller('todoController', function(){
 	this.new = {};
-	this.todos = [{"Title" : "Prepare summary report", "Description" : "The work is to prepare a summary report!","Status":"1"},
-			{"Title" : "CISCO - Review the BOM", "Description" : "The task is to review the BOM for CISCO","Status":"0"},
-			{"Title" : "IC Branch - Job Completion", "Description" : "Verify the work status and provide the cheque.","Status":"1"},
-			{"Title" : "Mail the Department Heads - Public Folder", "Description" : "Email all of the department heads about the policy!","Status":"2"},
-			{"Title" : "Canias Implementation in JIZAN", "Description" : "Full implementation of Canias in JIZAN!","Status":"0"}];
+	this.todos = [{"Title" : "Prepare summary report", "Description" : "The work is to prepare a summary report!","Status":100},
+			{"Title" : "CISCO - Review the BOM", "Description" : "The task is to review the BOM for CISCO","Status":25},
+			{"Title" : "IC Branch - Job Completion", "Description" : "Verify the work status and provide the cheque.","Status":80},
+			{"Title" : "Mail the Department Heads - Public Folder", "Description" : "Email all of the department heads about the policy!","Status":66},
+			{"Title" : "Canias Implementation in JIZAN", "Description" : "Full implementation of Canias in JIZAN!","Status":12}];
 	
 	this.add = function() {
-		this.new.Status = 0;
+		
 		this.todos.push(this.new);
 		this.new = {};
+		$('.progress').progress();
+	}
+
+	this.refresh = function(){
+		$('.progress').progress();
+	}
+
+	this.delete = function(){
+		this.todos.delete(this.new);
 	}
 
 });
+
